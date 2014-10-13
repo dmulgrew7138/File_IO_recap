65f0e2688a33ac604d63047ff56f88ef30203eb0
James Robinson <jrobinson@soham-college.org.uk>
2014-10-13 22:19:54 +0100

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,4 @@
+#Create a text file in Notepad of user names and passwords. Ask the user to enter their user name and password. The system should read in the details from the file, then return one of these messages:
+#Your login is successful (correct username and password)
+#Incorrect password (username exists but password does not match)
+#This username does not exist (username does not exist in the file)
