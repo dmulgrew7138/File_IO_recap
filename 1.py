615891719bcd5199e879590fc3b702ac6631f1ef
dmulgrew7138 <dmulgrew7138@sohamcollege.org.uk>
2014-10-14 09:43:22 +0100

    Update 1.py
    happy shopping

--- 1.py
+++ 1.py
@@ -1,1 +1,5 @@
 #Write a shopping list in Notepad and save it as 'shopping.txt'. Write a program to read in the shopping list from the file and display it on the screen.
+def shopping_list():
+    with open("shoppinglist.txt" , mode="r", encoding ="utf-8") as my_file:
+        for line in my_file:
+            print(line)
